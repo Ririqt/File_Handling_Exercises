6903e65ea4644c6e0b264967f442eb1bb0361c01
Rei Rigos <mcrei1018@gmail.com>
2024-04-20 19:30:19 +0800

    Using a loop to cover the Even Numbers

--- Numbers_Odd_Even.py
+++ Numbers_Odd_Even.py
@@ -7,4 +7,5 @@ even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
 
 with open("even_numbers.txt", "w") as even_file:
-    
+    for num in even_numbers:
+        
