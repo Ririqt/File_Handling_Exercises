cf7fa48367f6a796123b22582baaf9297e7577e4
Rei Rigos <mcrei1018@gmail.com>
2024-04-20 21:31:58 +0800

    Setting Condition to get the highest gwa

--- File_Student_Highest_GWA.py
+++ File_Student_Highest_GWA.py
@@ -7,3 +7,5 @@ name_student = ''
 for data in datas:
     name, general_weighted_average = data.strip().split(',')
     general_weighted_average = float(general_weighted_average)
+
+    if general_weighted_average <= highest_general_weighted_average:
