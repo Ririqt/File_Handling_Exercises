8506712fb2d26431a31d08b42700ba6e9169005d
Rei Rigos <mcrei1018@gmail.com>
2024-04-20 19:28:35 +0800

    Putting the Even Numbers into the Even File

--- Numbers_Odd_Even.py
+++ Numbers_Odd_Even.py
@@ -5,3 +5,6 @@ numbers = [int(num) for num in numbers]
 
 even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
+
+with open("even_numbers.txt", "w") as even_file:
+    
