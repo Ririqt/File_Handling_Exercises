a75dee85d56fb679667e8ffe87fec9d252279376
Rei Rigos <mcrei1018@gmail.com>
2024-04-20 19:53:33 +0800

    Pseudocode

--- Numbers_Odd_Even.py
+++ Numbers_Odd_Even.py
@@ -2,15 +2,19 @@
 with open("numbers.txt", "r") as file:
     numbers = file.read().split()
 
+# Storing numbers making num 'int'
 numbers = [int(num) for num in numbers]
 
+# Even and Odd Numbers
 even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
 
+# Creating Even File Numbers
 with open("even_numbers.txt", "w") as even_file:
     for num in even_numbers:
         even_file.write(str(num) + "\n")
 
+# Creating Odd File Numbers
 with open("odd_numbers.txt", "w") as odd_file:
     for num in odd_numbers:
         odd_file.write(str(num) + "\n")
