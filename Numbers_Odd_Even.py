b0d431589f05de9c83830b109fc77f94ca558aef
Rei Rigos <mcrei1018@gmail.com>
2024-04-20 19:20:18 +0800

    Revert "Storing Odd Numbers for every number"
    This reverts commit 2b4547ba87d125ce9dfb025ffb57c660f6cbe62e.

--- Numbers_Odd_Even.py
+++ Numbers_Odd_Even.py
@@ -4,4 +4,3 @@ with open("numbers.txt", "r") as file:
 numbers = [int(num) for num in numbers]
 
 even_numbers = [num for num in numbers if num % 2 == 0]
-odd_numbers = [num for num in numbers if num % 2 != 0]
