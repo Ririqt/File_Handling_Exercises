bcea67d066cc8d161313558e24de58bad50a58fe
Rei Rigos <mcrei1018@gmail.com>
2024-04-20 21:49:12 +0800

    Pseudocode

--- File_Student_Highest_GWA.py
+++ File_Student_Highest_GWA.py
@@ -1,15 +1,20 @@
+# Reading Per Line in the Files' Data
 with open("students_gwa.txt","r") as file:
     datas = file.readlines()
 
+# Storing the Data
 highest_general_weighted_average = float('inf')
 name_student = ''
 
+# Separating the Data from the file
 for data in datas:
     name, general_weighted_average = data.strip().split(',')
     general_weighted_average = float(general_weighted_average)
 
+# Getting the Highest General Weighted Average
     if general_weighted_average <= highest_general_weighted_average:
         highest_general_weighted_average = general_weighted_average
         name_student = name
 
+# Displaying the Student who got the highest GWA
 print(f"The highest GWA is '{highest_general_weighted_average}' which is student '{name_student}'.")
