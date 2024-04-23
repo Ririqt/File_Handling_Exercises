a60a94c727eae5b2a31d28176a822ba5e8fd04a6
Rei Rigos <mcrei1018@gmail.com>
2024-04-23 16:53:25 +0800

    Creating a File of 'triple.txt'

--- even_squares_and_odd_cubes.py
+++ even_squares_and_odd_cubes.py
@@ -7,6 +7,9 @@ odd_integers = [number for number in integers if number % 2 != 0]
 even_squares = [number ** 2 for number in even_integers]
 odd_cubes = [number ** 3 for number in odd_integers]
 
-with open('double.txt', 'w') as file:
+with open("double.txt", "w") as file:
     for square in even_squares:
         file.write(f"{square}\n")
+
+with open("triple.txt", "w") as file:
+    
