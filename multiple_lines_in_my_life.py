48546249d3585b95621f1c82efbb4a91ea6df18a
Rei Rigos <mcrei1018@gmail.com>
2024-04-23 16:05:52 +0800

    Pseudocode

--- multiple_lines_in_my_life.py
+++ multiple_lines_in_my_life.py
@@ -1,11 +1,15 @@
+# Writing into the File
 with open("mylife.txt","w") as file:
 
+# Creating a Loop for Entering Lines
     while True:
         enter_line = input("Enter Line: ")
         file.write(enter_line + "\n")
         more_lines = input("Are there more lines y/n? ")
 
+        # Exiting the Loop
         if more_lines.lower() != 'y':
             break
 
+# Display of Success Text
 print("The text has written into the file of 'mylife.txt'.")
