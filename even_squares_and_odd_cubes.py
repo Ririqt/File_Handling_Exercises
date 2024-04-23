8894fbdaf5671399e2f36220bcb2143f56c0641e
Rei Rigos <mcrei1018@gmail.com>
2024-04-23 16:49:50 +0800

    Using a loop to get the Squares

--- even_squares_and_odd_cubes.py
+++ even_squares_and_odd_cubes.py
@@ -8,4 +8,5 @@ even_squares = [number ** 2 for number in even_integers]
 odd_cubes = [number ** 3 for number in odd_integers]
 
 with open('double.txt', 'w') as file:
-    
+    for square in even_squares:
+        
