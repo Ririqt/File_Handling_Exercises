24695440ca3031a3165da20e471a3a4878222cf7
Rei Rigos <mcrei1018@gmail.com>
2024-04-23 15:49:23 +0800

    Prompt the User to Enter more lines

--- multiple_lines_in_my_life.py
+++ multiple_lines_in_my_life.py
@@ -3,3 +3,5 @@ with open("mylife.txt","w") as file:
     while True:
         enter_line = input("Enter Line: ")
         file.write(enter_line + "\n")
+        more_lines = input("Are there more lines y/n? ")
+        
