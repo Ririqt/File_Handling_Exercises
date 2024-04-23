6d6fb855e2c70be3bfd1cdb7f871791557e02c42
Rei Rigos <mcrei1018@gmail.com>
2024-04-23 16:43:46 +0800

    Separating the Odd Integers

--- even_squares_and_odd_cubes.py
+++ even_squares_and_odd_cubes.py
@@ -2,3 +2,4 @@ with open("integers.txt", 'r') as file:
     integers = [int(line.strip()) for line in file]
 
 even_integers = [number for number in integers if number % 2 == 0]
+odd_integers = [number for number in integers if number % 2 != 0]
